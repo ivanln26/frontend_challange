915a6941893218a65b8f2d2da21ea1ae7910e49b
Ivan Nuñez <ivanluis.cba@gmail.com>
2021-02-23 22:42:37 -0300

    Remove form tag

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,7 @@ import { encode } from "../encoding/encode";
 
 const onSubmit = (data, setCode, sheet) => {
   const code = encode(data);
-  data = {...data, code: code}
+  data = { ...data, code: code }
   setCode(code);
   (async function () {
     await sheet.addRow(data);
@@ -36,54 +36,53 @@ function Form() {
   useEffect(() => {
     (async function () {
       const doc = new GoogleSpreadsheet("1U3mKKdMzBvXqIDXii_wnzaTOGB35yX0gACbL5_jPb5g");
-        doc.useServiceAccountAuth({
-          client_email: credentials["client_email"],
-          private_key: credentials["private_key"],
-        });
-        await doc.loadInfo();
-        const sh = doc.sheetsById[0];
-        setSheet(sh);
+      doc.useServiceAccountAuth({
+        client_email: credentials["client_email"],
+        private_key: credentials["private_key"],
+      });
+      await doc.loadInfo();
+      const sh = doc.sheetsById[0];
+      setSheet(sh);
     }());
   }, ['']);
 
   return (
     <div>
-      <form action="#" onSubmit={() => onSubmit(fields, setCode, sheet)}>
-        <input
-          onChange={e => updateField({ name: e.target.value })}
-          placeholder="Name"
-          required
-          type="text"
-          value={fields.name}
-        />
-        <br />
-        <input
-          onChange={e => updateField({ surname: e.target.value })}
-          placeholder="Surname"
-          required
-          type="text"
-          value={fields.surname}
-        />
-        <br />
-        Gender:
-        <select
-          onChange={e => updateField({ gender: e.target.value })}
-          value={fields.gender}
-        >
-          <option value="M">Male</option>
-          <option value="F">Female</option>
-        </select>
-        <br />
-        <input
-          onChange={e => updateField({ birthdate: e.target.value })}
-          placeholder="Birthdate"
-          required
-          type="text"
-          value={fields.birthdate}
-        />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+      <input
+        onChange={e => updateField({ name: e.target.value })}
+        placeholder="Name"
+        required
+        type="text"
+        value={fields.name}
+      />
+      <br />
+      <input
+        onChange={e => updateField({ surname: e.target.value })}
+        placeholder="Surname"
+        required
+        type="text"
+        value={fields.surname}
+      />
+      <br />
+      Gender:
+      <select
+        onChange={e => updateField({ gender: e.target.value })}
+        value={fields.gender}
+      >
+        <option value="M">Male</option>
+        <option value="F">Female</option>
+      </select>
+      <br />
+      <input
+        onChange={e => updateField({ birthdate: e.target.value })}
+        placeholder="Birthdate"
+        required
+        type="text"
+        value={fields.birthdate}
+      />
+      <br />
+      <button onClick={() => onSubmit(fields, setCode, sheet)}>Submit</button>
+      <br />
       Your tax code is: {code}
     </div>
   );
